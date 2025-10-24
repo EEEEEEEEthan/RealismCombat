b7440b8b82d990ffcbaf8ee1d5e97905d7595e3d
ethan <tyx193@live.cn>
2025-10-25 03:57:42 +0800

    Enhance CommandHandler to improve command handling and logging functionality. Implemented a logging mechanism to capture messages during command execution and updated command responses for better clarity.

--- Scripts/Commands/CommandHandler.gd
+++ Scripts/Commands/CommandHandler.gd
@@ -3,13 +3,22 @@ extends RefCounted
 class_name CommandHandler
 
 var _game: Game
+var logs: Array[String] = []
 
 func _init(game: Game) -> void:
 	_game = game
 
 func handle(command: String) -> String:
+	logs.clear()
+	_game.on_log.connect(_on_log)
 	if command == "game.check_status":
-		return "这个功能还没实现";
-	if command == StartNextCombat.NAME:
-		return StartNextCombat.new().execute()
-	return "unknown command: " + command
+		print("这个功能还没实现")
+	elif command == StartNextCombat.NAME:
+		await StartNextCombat.new().execute()
+	else:
+		print("unknown command: " + command)
+	_game.on_log.disconnect(_on_log)
+	return "\n".join(logs)
+
+func _on_log(msg: String) -> void:
+	logs.append(msg)
