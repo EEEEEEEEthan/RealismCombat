d74f099201bd9b05d95342ce29039fce0088a86c
ethan <tyx193@live.cn>
2025-10-25 02:23:08 +0800

    增加资源表

---
+++ Scripts/ResourceTable.gd
@@ -0,0 +1,8 @@
+extends RefCounted
+
+class_name ResourceTable
+
+const SCENE_EVENT = "res://Scenes/Event.tscn"
+const SCENE_GAME = "res://Scenes/Game.tscn"
+const SCENE_PROFILE = "res://Scenes/Profile.tscn"
+const ICON = "res://icon.svg"
