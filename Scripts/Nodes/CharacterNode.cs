25aab6db254ada51fee66d7048da5929c6212b60
ethan <tyx193@live.cn>
2025-10-27 01:40:18 +0800

    feat(Character): add Character scene and node script
    - Introduce a new Character scene with a Control node and background ColorRect.
    - Implement CharacterNode script to manage character data and update the displayed name.
    - Establish a connection between the scene and the script for dynamic name updates.

---
+++ Scripts/Nodes/CharacterNode.cs
@@ -0,0 +1,19 @@
+﻿using Godot;
+using RealismCombat.Data;
+namespace RealismCombat.Nodes;
+partial class CharacterNode : Control
+{
+	RichTextLabel richTextLabelName = null!;
+	CharacterData? characterData;
+	public CharacterData? CharacterData
+	{
+		get => characterData;
+		set
+		{
+			characterData = value;
+			if (value == null) return;
+			richTextLabelName.Text = value.name;
+		}
+	}
+	public override void _Ready() => richTextLabelName = GetNode<RichTextLabel>("ColorRectBackground/RichTextLabelName");
+}
