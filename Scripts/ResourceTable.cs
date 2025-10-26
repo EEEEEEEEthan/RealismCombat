0bb8cf8df31af16f9cc00625655c5fbaab3ea225
ethan <tyx193@live.cn>
2025-10-26 22:27:48 +0800

    fix: update battlePrepareScene path to point to the correct Prepare.tscn file

--- Scripts/ResourceTable.cs
+++ Scripts/ResourceTable.cs
@@ -1,7 +1,7 @@
 ﻿namespace RealismCombat;
 public static class ResourceTable
 {
-	public const string battlePrepareScene = "res://Scenes/BattlePrepareScene.tscn";
+	public const string battlePrepareScene = "res://Scenes/Prepare.tscn";
 	public const string gameScene = "res://Scenes/Game.tscn";
 	public const string combatScene = "res://Scenes/Combat.tscn";
 	public const string menuScene = "res://Scenes/MainMenu.tscn";
