b465c68b50906191ebbb961940e8dfd18ff20dd4
ethan <ethan.tang@coconutislandstudio.com>
2025-10-27 11:15:41 +0800

    存档记录执行中的行动

--- Scripts/Data/CombatData.cs
+++ Scripts/Data/CombatData.cs
@@ -3,13 +3,20 @@ using System.IO;
 namespace RealismCombat.Data;
 class CombatData
 {
-	public readonly List<CharacterData> characters = new();
+	public readonly List<CharacterData> characters = [];
+	public readonly Dictionary<string, string> tempData = new();
 	public byte state;
 	public CombatData(DataVersion version, BinaryReader reader)
 	{
 		state = reader.ReadByte();
-		var count = reader.ReadByte();
-		for (var i = count; i-- > 0;) characters.Add(new(version: version, reader: reader));
+		{
+			var count = reader.ReadByte();
+			for (var i = count; i-- > 0;) characters.Add(new(version: version, reader: reader));
+		}
+		{
+			var count = reader.ReadByte();
+			for (var i = 0; i < count; ++i) tempData[reader.ReadString()] = reader.ReadString();
+		}
 	}
 	public CombatData() { }
 	public void Serialize(BinaryWriter writer)
@@ -17,5 +24,11 @@ class CombatData
 		writer.Write(state);
 		writer.Write((byte)characters.Count);
 		foreach (var character in characters) character.Serialize(writer);
+		writer.Write(tempData.Count);
+		foreach (var (key, value) in tempData)
+		{
+			writer.Write(key);
+			writer.Write(value);
+		}
 	}
 }
--- Scripts/StateMachine/CombatStates/ActionState.cs
+++ Scripts/StateMachine/CombatStates/ActionState.cs
@@ -8,11 +8,13 @@ using RealismCombat.StateMachine.GameStates;
 namespace RealismCombat.StateMachine.CombatStates;
 class ActionState(CombatState combatState, CombatData combatData, CharacterData actor) : CombatChildState(combatState: combatState, combatData: combatData)
 {
+	const string key = "ActionCommand";
 	public readonly CharacterData actor = actor;
+	bool executing;
 	public override string Name => "进行行动";
 	public override IReadOnlyDictionary<string, Func<IReadOnlyDictionary<string, string>, Command>> GetCommandGetters()
 	{
-		if (!actor.PlayerControlled) return new Dictionary<string, Func<IReadOnlyDictionary<string, string>, Command>>();
+		if (!actor.PlayerControlled || executing) return new Dictionary<string, Func<IReadOnlyDictionary<string, string>, Command>>();
 		return new Dictionary<string, Func<IReadOnlyDictionary<string, string>, Command>>
 		{
 			{ AttackCommand.name, arguments => new AttackCommand(actionState: this, arguments: arguments) },
@@ -20,23 +22,28 @@ class ActionState(CombatState combatState, CombatData combatData, CharacterData 
 	}
 	public override void Update(double dt)
 	{
+		if (combatData.tempData.TryGetValue(key: key, value: out var command))
+		{
+			if (executing) return; // 指令正在执行(播放执行动画)
+			Execute(command); // 读档
+		}
 		if (actor.PlayerControlled) return;
 		var enemies = combatData.characters.Where(c => c.team != actor.team && !c.Dead).ToList();
-		if (enemies.Count > 0)
-		{
-			var target = enemies[Random.Shared.Next(enemies.Count)];
-			var bodyParts = new[] { "head", "chest", "leftArm", "rightArm", "leftLeg", "rightLeg", };
-			var attackPart = bodyParts[Random.Shared.Next(bodyParts.Length)];
-			var targetPart = bodyParts[Random.Shared.Next(bodyParts.Length)];
-			var attackCommand = new AttackCommand(actionState: this,
-				arguments: new Dictionary<string, string>
-				{
-					{ "target", target.name },
-					{ "attackerPart", attackPart },
-					{ "targetPart", targetPart },
-				});
-			attackCommand.Execute();
-		}
+		var target = enemies[Random.Shared.Next(enemies.Count)];
+		var bodyParts = new[] { "head", "chest", "leftArm", "rightArm", "leftLeg", "rightLeg", };
+		var attackPart = bodyParts[Random.Shared.Next(bodyParts.Length)];
+		var targetPart = bodyParts[Random.Shared.Next(bodyParts.Length)];
+		command = $"{AttackCommand.name} target {target.name} attackerPart {attackPart} targetPart {targetPart}";
+		Execute(command);
+	}
+	void Execute(string command)
+	{
+		if (executing) throw new("指令正在执行");
+		combatData.tempData[key] = command;
+		combatState.gameState.Save();
+		executing = true;
+		ExecuteCommand(command);
+		combatData.tempData.Remove(key);
 	}
 	private protected override void OnExit() { }
 	private protected override string GetStatus() => "行动中";
