0e6db104fdd3c68671f06b9ffdb4906d9ae496da
ethan <tyx193@live.cn>
2025-10-29 22:20:44 +0800

    regen resourcetable

--- Scripts/ResourceTable.cs
+++ Scripts/ResourceTable.cs
@@ -9,18 +9,11 @@ public static class ResourceTable
 	public const string dialogueScene = "res://Scenes/Dialogue.tscn";
 	public const string emptyIcon = "res://Textures/EmptyIcon.png";
 	#region autogen_scenes
-	public const string character = "res://Scenes/Character.tscn";
-	public const string combat = "res://Scenes/Combat.tscn";
 	public const string componentsOption = "res://Scenes/Components/Option.tscn";
 	public const string componentsPrinterlabel = "res://Scenes/Components/PrinterLabel.tscn";
-	public const string dialogue = "res://Scenes/Dialogue.tscn";
-	public const string dialoguesActiondialogue = "res://Scenes/Dialogues/ActionDialogue.tscn";
 	public const string dialoguesGenericdialogue = "res://Scenes/Dialogues/GenericDialogue.tscn";
 	public const string dialoguesMenudialogue = "res://Scenes/Dialogues/MenuDialogue.tscn";
 	public const string game = "res://Scenes/Game.tscn";
-	public const string mainmenu = "res://Scenes/MainMenu.tscn";
-	public const string prepare = "res://Scenes/Prepare.tscn";
 	public const string programroot = "res://Scenes/ProgramRoot.tscn";
-	public const string propertydrawer = "res://Scenes/PropertyDrawer.tscn";
 	#endregion autogen_scenes
 }
