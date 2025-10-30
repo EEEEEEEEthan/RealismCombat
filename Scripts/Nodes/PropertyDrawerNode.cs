14abe9287b439b36d23443ea8bafdb3839e4f552
ethan <ethan.tang@coconutislandstudio.com>
2025-10-30 10:25:35 +0800

    更新角色和属性绘制场景，调整UI布局和资源引用
    - 修改Character.tscn中的偏移量以优化UI布局
    - 更新PropertyDrawer.tscn，添加新的纹理资源并调整节点结构
    - 在PropertyDrawerNode.cs中更改value类型为Control，并更新处理逻辑以适应新结构

--- Scripts/Nodes/PropertyDrawerNode.cs
+++ Scripts/Nodes/PropertyDrawerNode.cs
@@ -1,4 +1,3 @@
-using System.Text;
 using Godot;
 namespace RealismCombat.Nodes;
 partial class PropertyDrawerNode : Node
@@ -6,13 +5,10 @@ partial class PropertyDrawerNode : Node
 	public (int value, int max) property = (10, 10);
 	public string title;
 	[Export] Label name = null!;
-	[Export] Label value = null!;
+	[Export] Control value = null!;
 	public override void _Process(double delta)
 	{
 		name.Text = title;
-		var builder = new StringBuilder();
-		for (var i = 0; i < property.value; i++) builder.Append('I');
-		for (var i = property.value; i < property.max; i++) builder.Append('.');
-		value.Text = builder.ToString();
+		value.CustomMinimumSize = value.CustomMinimumSize with { X = (int)(property.value * 20f / property.max), };
 	}
 }
