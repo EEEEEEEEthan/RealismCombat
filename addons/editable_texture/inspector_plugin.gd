c4783c9f75ea93254f4b505b94bb0edeaaf6d2f0
ethan <ethan.tang@coconutislandstudio.com>
2026-01-07 15:44:26 +0800

    Remove reference to _raw_texture in _parse_property function, streamlining property checks for EditableTexture.

--- addons/editable_texture/inspector_plugin.gd
+++ addons/editable_texture/inspector_plugin.gd
@@ -13,7 +13,7 @@ func _parse_property(
 	_hint_string: String,
 	_usage_flags: PropertyUsageFlags,
 	_wide: bool) -> bool:
-	return name == "_raw_texture" or name == "_hex_data"
+	return name == "_hex_data"
 func _parse_begin(object: Object) -> void:
 	if object is not EditableTexture:
 		return
