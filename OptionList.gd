8fab7e94fae12d36bb15f241cf05d561d2ec6d08
ethan <tyx1993@live.cn>
2026-01-13 01:17:58 +0800

    重构 OptionList: 将选项变量重命名为 _选项，添加 _禁用选项 属性以支持禁用功能，并更新相关逻辑以适应新命名。

--- OptionList.gd
+++ OptionList.gd
@@ -11,9 +11,14 @@ static var _空样式: StyleBoxEmpty = StyleBoxEmpty.new()
 		视区数量 = 值
 		_延迟更新视区()
 
-@export var 选项: PackedStringArray:
+@export var _选项: PackedStringArray:
 	set(值):
-		选项 = 值
+		_选项 = 值
+		_延迟更新视区()
+
+@export var _禁用选项: int:
+	set(值):
+		_禁用选项 = 值
 		_延迟更新视区()
 
 @export_range(1, 7) var 空余数量: int = 1:
@@ -61,15 +66,12 @@ func _延迟更新主题() -> void:
 		_更新主题()
 
 func _更新主题() -> void:
-	# 更新透明图标尺寸以匹配当前图标
 	if _当前图标:
 		var 图标尺寸 = max(_当前图标.get_width(), _当前图标.get_height())
 		if _透明图标 == null or _透明图标.get_width() != 图标尺寸:
 			var 图片 = Image.create(图标尺寸, 图标尺寸, false, Image.FORMAT_RGBA8)
 			图片.fill(Color.TRANSPARENT)
 			_透明图标 = ImageTexture.create_from_image(图片)
-	
-	# 更新所有按钮的图标状态
 	_更新所有按钮图标()
 
 func _更新视区() -> void:
@@ -90,27 +92,36 @@ func _更新视区() -> void:
 		if _透明图标:
 			按钮.icon = _透明图标
 		_选项容器.add_child(按钮)
-	var 可见数量 = min(视区数量, len(选项))
+	var 可见数量 = min(视区数量, len(_选项))
 	for 索引 in range(可见数量):
+		var 按钮 = _选项容器.get_child(索引) as Button
 		if 索引 == 0 and _视区第一个编号 > 0:
-			_选项容器.get_child(索引).text = "..."
-		elif 视区数量 - 1 == 索引 and _视区第一个编号 + 视区数量 < len(选项):
-			_选项容器.get_child(索引).text = "..."
-		elif 索引 + _视区第一个编号 < len(选项):
-			_选项容器.get_child(索引).text = 选项[索引 + _视区第一个编号]
+			按钮.text = "..."
+		elif 视区数量 - 1 == 索引 and _视区第一个编号 + 视区数量 < len(_选项):
+			按钮.text = "..."
+		elif 索引 + _视区第一个编号 < len(_选项):
+			按钮.text = _选项[索引 + _视区第一个编号]
 		else:
-			_选项容器.get_child(索引).text = ""
+			按钮.text = ""
+		按钮.disabled = ((索引 & _禁用选项) != 0)
 	for 索引 in range(可见数量, 视区数量 - 可见数量):
 		(_选项容器.get_child(索引) as Button).text = ""
 	_更新所有按钮图标()
 
 func _on_button_focus_entered(按钮: Button) -> void:
-	if _当前图标:
-		按钮.icon = _当前图标
+	按钮.icon = _当前图标
+	var 按钮下标 = 按钮.get_index()
+	if 按钮下标 == 0 and _视区第一个编号 > 0:
+		_视区第一个编号 -= 1
+		按钮.get_parent().get_child(1).grab_focus()
+		call_deferred("_更新视区")
+	elif 按钮下标 == 视区数量 - 1 and _视区第一个编号 + 视区数量 < len(_选项):
+		_视区第一个编号 += 1
+		按钮.get_parent().get_child(按钮下标 - 1).grab_focus()
+		call_deferred("_更新视区")
 
 func _on_button_focus_exited(按钮: Button) -> void:
-	if _透明图标:
-		按钮.icon = _透明图标
+	按钮.icon = _透明图标
 
 func _on_button_mouse_entered(按钮: Button) -> void:
 	按钮.grab_focus()
@@ -121,8 +132,6 @@ func _更新所有按钮图标() -> void:
 		var 按钮 = _选项容器.get_child(i) as Button
 		if 按钮:
 			if 按钮.has_focus():
-				if _当前图标:
-					按钮.icon = _当前图标
+				按钮.icon = _当前图标
 			else:
-				if _透明图标:
-					按钮.icon = _透明图标
+				按钮.icon = _透明图标
