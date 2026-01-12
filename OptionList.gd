d0aaef473e0030fbb6ed978ee715e33c3dd7c56d
ethan <tyx1993@live.cn>
2026-01-13 01:28:30 +0800

    重构 OptionList: 将延迟更新方法替换为尝试更新方法，简化视区和主题更新逻辑，并添加按钮创建和计算选项索引的辅助函数

--- OptionList.gd
+++ OptionList.gd
@@ -9,29 +9,24 @@ static var _空样式: StyleBoxEmpty = StyleBoxEmpty.new()
 @export_range(3, 16) var 视区数量: int = 8:
 	set(值):
 		视区数量 = 值
-		_延迟更新视区()
+		_尝试更新(_更新视区)
 
 @export var _选项: PackedStringArray:
 	set(值):
 		_选项 = 值
-		_延迟更新视区()
+		_尝试更新(_更新视区)
 
 @export var _禁用选项: int:
 	set(值):
 		_禁用选项 = 值
-		_延迟更新视区()
-
-@export_range(1, 7) var 空余数量: int = 1:
-	set(值):
-		空余数量 = 值
-		_延迟更新视区()
+		_尝试更新(_更新视区)
 
 @export_group("Theme Overrides")
 @export_subgroup("icons")
 @export var 指示器图标: Texture2D = null:
 	set(值):
 		指示器图标 = 值
-		_延迟更新主题()
+		_尝试更新(_更新主题)
 
 signal 当聚焦于选项(选项索引: int)
 signal 当选择选项(选项索引: int)
@@ -57,16 +52,12 @@ func _ready() -> void:
 	_选项容器 = VBoxContainer.new()
 	_选项容器.set_anchors_and_offsets_preset(Control.PRESET_FULL_RECT)
 	add_child(_选项容器)
-	_延迟更新主题()
-	_延迟更新视区()
+	_尝试更新(_更新主题)
+	_尝试更新(_更新视区)
 
-func _延迟更新视区() -> void:
+func _尝试更新(更新函数: Callable) -> void:
 	if is_node_ready():
-		_更新视区()
-
-func _延迟更新主题() -> void:
-	if is_node_ready():
-		_更新主题()
+		更新函数.call()
 
 func _更新主题() -> void:
 	if _当前图标:
@@ -82,20 +73,7 @@ func _更新视区() -> void:
 	for 索引 in range(节点数量 - 视区数量):
 		_选项容器.get_child(节点数量 - 索引 - 1).queue_free()
 	for 索引 in range(视区数量 - 节点数量):
-		var 按钮 = Button.new()
-		按钮.focus_entered.connect(_on_button_focus_entered.bind(按钮))
-		按钮.focus_exited.connect(_on_button_focus_exited.bind(按钮))
-		按钮.mouse_entered.connect(_on_button_mouse_entered.bind(按钮))
-		按钮.pressed.connect(_on_button_pressed.bind(按钮))
-		按钮.add_theme_stylebox_override("normal", _空样式)
-		按钮.add_theme_stylebox_override("hover", _空样式)
-		按钮.add_theme_stylebox_override("pressed", _空样式)
-		按钮.add_theme_stylebox_override("disabled", _空样式)
-		按钮.add_theme_stylebox_override("focus", _空样式)
-		按钮.alignment = HORIZONTAL_ALIGNMENT_LEFT
-		if _透明图标:
-			按钮.icon = _透明图标
-		_选项容器.add_child(按钮)
+		_选项容器.add_child(_创建按钮())
 	var 可见数量 = min(视区数量, len(_选项))
 	for 索引 in range(可见数量):
 		var 按钮 = _选项容器.get_child(索引) as Button
@@ -108,7 +86,7 @@ func _更新视区() -> void:
 		else:
 			按钮.text = ""
 		按钮.disabled = ((索引 & _禁用选项) != 0)
-	for 索引 in range(可见数量, 视区数量 - 可见数量):
+	for 索引 in range(可见数量, 视区数量):
 		(_选项容器.get_child(索引) as Button).text = ""
 	_更新所有按钮图标()
 
@@ -124,7 +102,7 @@ func _on_button_focus_entered(按钮: Button) -> void:
 		按钮.get_parent().get_child(按钮下标 - 1).grab_focus()
 		call_deferred("_更新视区")
 	else:
-		var 选项索引 = 按钮下标 + _视区第一个编号
+		var 选项索引 = _计算选项索引(按钮下标)
 		if 选项索引 >= 0 and 选项索引 < len(_选项):
 			print("当聚焦于选项: ", _选项[选项索引])
 			当聚焦于选项.emit(选项索引)
@@ -137,10 +115,29 @@ func _on_button_mouse_entered(按钮: Button) -> void:
 
 func _on_button_pressed(按钮: Button) -> void:
 	var 按钮下标 = 按钮.get_index()
-	var 选项索引 = 按钮下标 + _视区第一个编号
+	var 选项索引 = _计算选项索引(按钮下标)
 	if not 按钮.disabled and 选项索引 >= 0 and 选项索引 < len(_选项) and not 按钮.text.begins_with("..."):
-		print("选择选项: ", _选项[选项索引])
+		print("当选择选项: ", _选项[选项索引])
 		当选择选项.emit(选项索引)
+
+func _创建按钮() -> Button:
+	var 按钮 = Button.new()
+	按钮.focus_entered.connect(_on_button_focus_entered.bind(按钮))
+	按钮.focus_exited.connect(_on_button_focus_exited.bind(按钮))
+	按钮.mouse_entered.connect(_on_button_mouse_entered.bind(按钮))
+	按钮.pressed.connect(_on_button_pressed.bind(按钮))
+	按钮.add_theme_stylebox_override("normal", _空样式)
+	按钮.add_theme_stylebox_override("hover", _空样式)
+	按钮.add_theme_stylebox_override("pressed", _空样式)
+	按钮.add_theme_stylebox_override("disabled", _空样式)
+	按钮.add_theme_stylebox_override("focus", _空样式)
+	按钮.alignment = HORIZONTAL_ALIGNMENT_LEFT
+	if _透明图标:
+		按钮.icon = _透明图标
+	return 按钮
+
+func _计算选项索引(按钮下标: int) -> int:
+	return 按钮下标 + _视区第一个编号
 
 func _更新所有按钮图标() -> void:
 	var 节点数量 = _选项容器.get_child_count()
