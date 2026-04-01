7f3eb0ef98bab695c5c1e858ef553813c4d6d50f
ethan <ethan.tang@coconutislandstudio.com>
2026-04-01 20:37:08 +0800

    Add audio feedback to CustomButton on focus
    Enhanced the CustomButton class by integrating an AudioStreamPlayer that plays a sound when the button gains focus. Updated the test scene to include the new CustomButton instance, ensuring audio feedback is part of the user interaction experience.

--- custom_button.gd
+++ custom_button.gd
@@ -1,12 +1,26 @@
 extends Button
 class_name CustomButton
 
+var _player: AudioStreamPlayer:
+	get:
+		if not _player:
+			_player = AudioStreamPlayer.new()
+			_player.stream = ResourceLoader.load("res://beep.mp3")
+			if not _player.stream:
+				push_error("audio missing: beep");
+			add_child(_player)
+		return _player
+
 func _ready() -> void:
 	connect("mouse_entered", _on_mouse_entered)
 	connect("mouse_exited", _on_mouse_exited)
+	connect("focus_entered", _on_focus_entered)
 
 func _on_mouse_entered() -> void:
 	grab_focus()
 
 func _on_mouse_exited() -> void:
 	release_focus()
+
+func _on_focus_entered() -> void:
+	_player.play()
