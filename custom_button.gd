f190f8eee7c22dcb02ee7d7dbef24843eb5c9a94
ethan <ethan.tang@coconutislandstudio.com>
2026-04-03 17:33:43 +0800

    Refactor CustomButton and update scene properties
    Removed the mouse exit signal connection from CustomButton and adjusted focus handling on button press. Updated option_list scene to include mouse filter settings for better interaction. Changed the hover pressed style in the theme to a new texture resource, enhancing visual feedback for button states.

--- custom_button.gd
+++ custom_button.gd
@@ -3,19 +3,15 @@ class_name CustomButton
 
 func _ready() -> void:
 	connect(&"mouse_entered", _on_mouse_entered)
-	connect(&"mouse_exited", _on_mouse_exited)
 	connect(&"focus_entered", _on_focus_entered)
 	connect(&"pressed", _on_pressed)
 
 func _on_mouse_entered() -> void:
 	grab_focus()
 
-func _on_mouse_exited() -> void:
-	release_focus()
-
 func _on_focus_entered() -> void:
 	AudioManager.play_beep()
 
 func _on_pressed() -> void:
-	print(11111111)
 	AudioManager.play_selection()
+	grab_focus()
