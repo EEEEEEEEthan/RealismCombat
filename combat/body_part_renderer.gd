e00c396b379e5638c3d5dd887c1c5b06d6b4ad3c
ethan <tyx1993@live.cn>
2026-04-04 13:14:22 +0800

    Add theme atlas resources and update theme configuration
    Introduced new theme atlas files (theme_atlas.aseprite and theme_atlas.png) along with their import settings. Updated theme.tres to reference the new texture resources and adjusted styles for progress bars. Refactored body_part_renderer.gd to enhance UI responsiveness and updated scene configurations for better layout management.

--- combat/body_part_renderer.gd
+++ combat/body_part_renderer.gd
@@ -1,29 +1,34 @@
 extends HBoxContainer
 
-@onready var part_label: Label = %Label
-
 @export var part: Enums.BodyPart = Enums.BodyPart.HEAD:
-	set(value):
-		part = value
+	set(v):
+		part = v
 		if is_node_ready():
 			_refresh_part_label()
 
+func update_hp(current_hp: int, max_hp: int, immedate: bool = false) -> void:
+	if immedate:
+		%ProgressBar.custom_minimum_size.x = max_hp * 2 + 1
+		%ProgressBar.max_value = max_hp
+		%ProgressBar.value = current_hp
+
 func _ready() -> void:
 	_refresh_part_label()
+	%ProgressBar.step = 1
 
 func _refresh_part_label() -> void:
 	match part:
 		Enums.BodyPart.HEAD:
-			part_label.text = "头部"
+			%Label.text = "头部"
 		Enums.BodyPart.CHEST:
-			part_label.text = "胸部"
+			%Label.text = "胸部"
 		Enums.BodyPart.RIGHT_HAND:
-			part_label.text = "右手"
+			%Label.text = "右手"
 		Enums.BodyPart.LEFT_HAND:
-			part_label.text = "左手"
+			%Label.text = "左手"
 		Enums.BodyPart.RIGHT_FOOT:
-			part_label.text = "右脚"
+			%Label.text = "右脚"
 		Enums.BodyPart.LEFT_FOOT:
-			part_label.text = "左脚"
+			%Label.text = "左脚"
 		_:
-			part_label.text = ""
+			%Label.text = ""
