d7703c178459863416ff666f6c3305ab74393f75
ethan <tyx1993@live.cn>
2026-04-07 23:54:14 +0800

    Refactor BodyPart class and remove CombatAction class
    Added a warning ignore for unused private class variable in BodyPart class. Deleted the CombatAction class and its associated UID file to streamline the combat action system, enhancing code clarity and maintainability.

--- game/character/body_part.gd
+++ game/character/body_part.gd
@@ -4,6 +4,7 @@ class_name BodyPart
 @export var part: Defs.BodyPart
 var hp: Property = Property.new(0, 0)
 
+@warning_ignore("unused_private_class_variable")
 @export var _hp: Vector2i:
 	get:
 		return Vector2i(int(hp.value), int(hp.max_value))
