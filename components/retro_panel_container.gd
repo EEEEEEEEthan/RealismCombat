5f1615d13c1bed3e4bc35403ee99332d805859f9
ethan <ethan.tang@coconutislandstudio.com>
2026-04-16 12:43:38 +0800

    Implement layout direction handling in RetroPanelContainer and remove test scene
    - Added _ready and _notification functions to manage layout direction changes dynamically.
    - Introduced _update_layout_direction method to adjust anchors based on layout direction (RTL or LTR).
    - Removed the obsolete test.tscn file to clean up the project structure.

--- components/retro_panel_container.gd
+++ components/retro_panel_container.gd
@@ -25,9 +25,7 @@ func _init() -> void:
 	_color_rect_1 = ColorRect.new()
 	_fill.add_child(_color_rect_1)
 	_color_rect_1.set_anchor_and_offset(SIDE_TOP, 0, 0)
-	_color_rect_1.set_anchor_and_offset(SIDE_RIGHT, 1, 1)
 	_color_rect_1.set_anchor_and_offset(SIDE_BOTTOM, 1, 0)
-	_color_rect_1.set_anchor_and_offset(SIDE_LEFT, 0, 0)
 	_color_rect_2 = ColorRect.new()
 	_fill.add_child(_color_rect_2)
 	_color_rect_2.set_anchor_and_offset(SIDE_TOP, 1, 0)
@@ -36,6 +34,21 @@ func _init() -> void:
 	_color_rect_2.set_anchor_and_offset(SIDE_LEFT, 0, 0)
 	_update_color()
 
+func _ready() -> void:
+	_update_layout_direction()
+
+func _notification(what: int) -> void:
+	if what == NOTIFICATION_LAYOUT_DIRECTION_CHANGED:
+		_update_layout_direction()
+
 func _update_color() -> void:
 	_color_rect_1.self_modulate = fill_color
 	_color_rect_2.self_modulate = fill_color
+
+func _update_layout_direction() -> void:
+	if is_layout_rtl():
+		_color_rect_1.set_anchor_and_offset(SIDE_RIGHT, 1, 0)
+		_color_rect_1.set_anchor_and_offset(SIDE_LEFT, 0, -1)
+	else:
+		_color_rect_1.set_anchor_and_offset(SIDE_RIGHT, 1, 1)
+		_color_rect_1.set_anchor_and_offset(SIDE_LEFT, 0, 0)
