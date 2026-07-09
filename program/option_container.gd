c9ad3ab6d5b251dee388eca043248ff2b796acc9
ethan <tyx1993@live.cn>
2026-07-09 23:04:11 +0800

    Refactor OptionContainer to improve viewport handling and arrow button functionality
    - Renamed `_up_arrow` to `_arrow_texture` for clarity.
    - Updated texture loading to use `Texture2D` and added assertions.
    - Introduced `viewport_begin` and `viewport_size` properties with export ranges.
    - Implemented `_true_viewport_begin` and `_true_viewport_size` for accurate child visibility.
    - Replaced `TextureRect` with `TextureButton` for up and down arrows, enhancing interactivity.
    - Updated scene file to reference the new script and adjusted properties accordingly.

--- program/option_container.gd
+++ program/option_container.gd
@@ -2,27 +2,65 @@
 extends VBoxContainer
 class_name OptionContainer
 
-var _up_arrow: AtlasTexture:
+var _arrow_texture: AtlasTexture:
 	get:
-		if not _up_arrow:
-			var image = ResourceLoader.load("uid://dkajou5ypu7d0") as Image
-			_up_arrow = AtlasTexture.new()
-			_up_arrow.atlas = image
-			_up_arrow.region = Rect2(21, 2, 8, 5)
-		return _up_arrow
+		if not _arrow_texture:
+			var image = ResourceLoader.load("uid://dkajou5ypu7d0") as Texture2D
+			assert(image)
+			_arrow_texture = AtlasTexture.new()
+			_arrow_texture.atlas = image
+			_arrow_texture.region = Rect2(21, 2, 8, 5)
+		return _arrow_texture
+
+@export_range(0, 32) var viewport_begin: int:
+	set(value):
+		viewport_begin = value
+		_update_viewport()
+
+@export_range(1, 8) var viewport_size: int:
+	set(value):
+		viewport_size = value
+		_update_viewport()
+
+var _true_viewport_begin: int:
+	get:
+		return 0 if viewport_begin == 0 else viewport_begin + 1
+
+var _true_viewport_size: int:
+	get:
+		var child_count = get_child_count()
+		if viewport_size >= child_count:
+			return viewport_size
+		if viewport_begin == 0:
+			return viewport_size - 1
+		return viewport_size - 2
+
+func _update_viewport() -> void:
+	if not is_node_ready(): await ready
+	var child_count := get_child_count()
+	var viewport_end = _true_viewport_begin + _true_viewport_size
+	for i in child_count:
+		get_child(i).visible = i >= _true_viewport_begin and i < viewport_end
+	_up_arrow.visible = viewport_begin > 0
+	_down_arrow.visible = viewport_end > child_count
+
+var _up_arrow: TextureButton
+var _down_arrow: TextureButton
 
 func _init() -> void:
-	var up_arrow = TextureRect.new()
-	up_arrow.texture = up_arrow
-	up_arrow.stretch_mode = TextureRect.STRETCH_KEEP_CENTERED
-	up_arrow.custom_minimum_size = Vector2(0, 8)
-	add_child(up_arrow, false, Node.INTERNAL_MODE_FRONT)
-	var down_arrow = TextureRect.new()
-	down_arrow.texture = up_arrow
-	down_arrow.stretch_mode = TextureRect.STRETCH_KEEP_CENTERED
-	down_arrow.custom_minimum_size = Vector2(0, 8)
-	down_arrow.flip_v = true
-	add_child(down_arrow, false, Node.INTERNAL_MODE_BACK)
+	_up_arrow = TextureButton.new()
+	_up_arrow.name = &"UpArrow"
+	_up_arrow.texture_normal = _arrow_texture
+	_up_arrow.stretch_mode = TextureButton.STRETCH_KEEP_CENTERED
+	_up_arrow.custom_minimum_size = Vector2(0, 8)
+	add_child(_up_arrow, false, Node.INTERNAL_MODE_FRONT)
+	_down_arrow = TextureButton.new()
+	_down_arrow.name = &"DownArrow"
+	_down_arrow.texture_normal = _arrow_texture
+	_down_arrow.stretch_mode = TextureButton.STRETCH_KEEP_CENTERED
+	_down_arrow.custom_minimum_size = Vector2(0, 8)
+	_down_arrow.flip_v = true
+	add_child(_down_arrow, false, Node.INTERNAL_MODE_BACK)
 
 func _exit_tree() -> void:
 	_up_arrow = null
