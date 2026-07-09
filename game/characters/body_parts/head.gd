a060d3669a496a959136ce8efffb3ba5b06c3971
ethan <ethan.tang@coconutislandstudio.com>
2026-07-09 18:10:07 +0800

    remake

--- game/characters/body_parts/head.gd
+++
@@ -1,6 +0,0 @@
-extends BodyPart
-class_name Head
-
-
-func part_name() -> StringName:
-	return &"头部"
--- game/save_slot_preview.gd
+++
@@ -1,7 +0,0 @@
-extends RefCounted
-class_name SaveSlotPreview
-
-## 无文件；此时 snapshot 为 null
-var is_empty: bool
-## 有档且可解析的存档头；有档但损坏为 null。与 is_empty 区分无档/损坏
-var snapshot: SaveSnapshot
